3812f8dabf95a31baf6580b6727aed7a1e2b25df
Danisa Valdez <danisa1223@gmail.com>
2026-09-15 20:57:05 -0400

    Remove The Attire tab; fix stale brief overview links (dead Lodging/Evening, add Journey)

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -44,7 +44,6 @@
       <h3>The Plans</h3>
       <ul>
         <li><a href="${prefix}dashboard/party.html">The Team</a></li>
-        <li><a href="${prefix}brief/the-attire.html">The Attire</a></li>
         <li><a href="${prefix}brief/the-journey.html">The Journey</a></li>
         <li><a href="${prefix}brief/the-logistics.html">The Logistics</a></li>
         <li><a href="${prefix}dashboard/day-of.html">The Day</a></li>
